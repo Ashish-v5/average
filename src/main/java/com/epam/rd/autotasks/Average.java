a4061792b53f39df7272e2aa00e75f78abc7419b
Ashish7429 <ashish.11916604@lpu.in>
2023-01-21 23:55:38 +0530

    done

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -6,6 +6,16 @@ public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Use Scanner methods to read input
+        int x = scanner.nextInt();
+        int sum = 0;
+        int count=0;
+        while (x!=0){
+            sum+=x;
+            count++;
+            x=scanner.nextInt();
+        }
+        int avg=sum/count;
+        System.out.println(avg);
 
     }
 
